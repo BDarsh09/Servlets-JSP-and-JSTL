57d73fbe7508d8cf1d3504f59135897c899ea264
Darshil Bavc <41076793+BDarsh09@users.noreply.github.com>
2019-02-08 23:37:27 +0530

    Create Servlet1.java

---
+++ Servlet-basic/Servlet1.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            out.println("<html><body>");
+            out.println("welcome to servlet");
+            out.println("</body></html>");
+            
+            out.close();
+        }
+    }
+
+  
+
+}
